113c1652ef300a11c8ed4a0e41e240d5e262ab73
garylai00000 <garylai00000@gmail.com>
2016-08-20 23:19:57 +0800

    update

--- vendor/github.com/DB/DBhandler.go
+++ vendor/github.com/DB/DBhandler.go
@@ -50,6 +50,57 @@ func GetTwoCards(MID string) [2]int{
 	return cards
 }
 
+////拿新的兩張手牌
+func NewTwoCards(MID string) [2]int{
+	var GameID int
+	GameID = 0;
+	var card1 int
+	var card2 int
+	cards := [2]int{-1, -1}
+	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
+	db.QueryRow("SELECT GameID, PlayerCard1, PlayerCard2 FROM sql6131889.GameAction WHERE MID = ? and Cancel = 0", MID ).Scan(&GameID, &card1, &card2)
+	if PlayerCard1 != 0{
+		//db.QueryRow("名字 FROM GameAction, 撲克牌參照表 WHERE MID = ? and PlayerCard1 = 編號", MID ).Scan(&card1name)
+		//db.QueryRow("名字 FROM GameAction, 撲克牌參照表 WHERE MID = ? and PlayerCard2 = 編號", MID ).Scan(&card2name)
+		rand.Seed(time.Now().UTC().UnixNano())
+		card1 = 1 + rand.Intn(52)
+		card2 = 1 + rand.Intn(52)
+		cards = [2]int{card1, card2}
+		db.Exec("UPDATE sql6131889.GameAction SET PlayerCard1 = ?, PlayerCard2 = ?  WHERE MID = ?", card1, card2, MID)
+		cards = [2]int{card1, card2}
+	}else{
+		rand.Seed(time.Now().UTC().UnixNano())
+		card1 = 1 + rand.Intn(52)
+		card2 = 1 + rand.Intn(52)
+		cards = [2]int{card1, card2}
+		db.Exec("INSERT INTO sql6131889.GameAction (ID, MID, GameID, PlayerX, Action, PlayerCard1, Cancel, PlayerCard2)VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 123, MID, 321, 11, 2, card1, 0, card2)
+	}
+	return cards
+}
+
+
+//拿新的五張牌並回傳
+func NewFiveCards(GameID int) [5]int{
+	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
+	rand.Seed(time.Now().UTC().UnixNano())
+	tablecards := [5]int{-1, -1, -1, -1, -1}
+	i := 0
+	for i < 5{
+		cards[i] = 1 + rand.Intn(52)
+		i = i + 1
+	}
+	db.Exec("UPDATE sql6131889.Game SET Card1 = ?, Card2 = ?, Card3 = ?, Card4 = ?, Card5 = ? WHERE ID = GameID", tablecards[0], tablecards[1], tablecards[2], tablecards[3], tablecards[4])
+	return tablecards
+}
+
+//回傳目前牌桌的牌
+func GetFiveCards(GameID int) [5]int{
+	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
+	db.QueryRow("SELECT Card1, Card2, Card3, Card4, Card5 FROM sql6131889.Game WHERE ID = GameID", MID ).Scan(&tablecards[0], &tablecards[1], &tablecards[2], &tablecards[3], &tablecards[4])
+	return tablecards
+}
+
+//丟卡片編號數字進來就給你卡片名子，如 Spade A
 func GetCardName(cardNo int) string{
 	var cardname string
 	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
@@ -69,4 +120,36 @@ func AddGameToken(RoomId int,addtoken int){
 	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
 	db.QueryRow("UPDATE sql6131889.Game SET GameToken=GameToken+? WHERE RoomID =?",addtoken,RoomId)
 	db.Close()
+}
+
+//洗牌啦，不回傳值
+func Shuffle(){
+	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
+	rand.Seed(time.Now().UTC().UnixNano())
+	i := 0
+	a := 0
+	b := 0
+	
+	cards := []int{
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+		14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+		25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+		36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+		47, 48, 49, 50, 51, 52}
+	
+	for i < 52{
+		a = rand.Intn(52)
+		b = rand.Intn(52) 
+		cards[a], cards[b] = Swap(cards[a], cards[b])
+		i = i +1 
+	}
+	i = 0
+	for i < 52{
+		db.QueryRow("UPDATE sql6131889.洗過的牌堆 SET 牌堆順序 = ? WHERE 編號 = ?", cards[i], i + 1)
+		i= i + 1
+	}
+}
+
+func Swap(x, y int) (int, int) {
+	return y, x
 }
