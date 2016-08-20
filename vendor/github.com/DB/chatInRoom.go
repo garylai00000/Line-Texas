872d02a0ce564efe3d016ec54d68a6ccb013d7a2
garylai00000 <garylai00000@gmail.com>
2016-08-21 05:52:08 +0800

    defuck

--- vendor/github.com/DB/chatInRoom.go
+++ vendor/github.com/DB/chatInRoom.go
@@ -222,19 +222,7 @@ func runCall(mID string,text string,gID int,rID int,mT int,nextS int) {
 }
 //棄牌
 func runFold(mID string,text string,gID int,mT int,nextS int){
-	// 	if i == pN - 1{ // one player left
-	// 	db.Exec("UPDATE sql6131889.Game SET GameStatus = ? WHERE RoomID = ? AND Cancel = ?",12,gID, 0)
-	// 	row,_ := db.Query("SELECT MID FROM sql6131889.GameAction WHERE GameID = ? AND Cancel = ?", gID, 0)
-	// 	for row.Next() {
-	// 		var mid1 string
-	// 		row.Scan(&mid1)
-	// 		if mid1 != mID{
-	// 			var n string
-	// 			db.QueryRow("SELECT UserName FROM sql6131889.User WHERE MID = ?",mID).Scan(&n)
-	// 			bot.SendText([]string{mid1}, n+" WIN")
-	// 		}
-	// 	}
-	// }
+
 	strID := os.Getenv("ChannelID")
 	numID, _ := strconv.ParseInt(strID, 10, 64) // string to integer
 	bot, _ = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
@@ -263,6 +251,8 @@ func runFold(mID string,text string,gID int,mT int,nextS int){
 		}
 	}
 	if counts == pN - 1 { // one player left
+		var pN int//遊戲人數
+		db.QueryRow("SELECT PlayerNum FROM sql6131889.Game WHERE ID = ? AND Cancel = ?",gID, 0).Scan(&pN)
 		db.Exec("UPDATE sql6131889.Game SET GameStatus = ? WHERE ID = ? AND Cancel = ?",12, gID, 0)
 		var md string // winner mid
 		db.QueryRow("SELECT MID FROM sql6131889.GameAction WHERE PlayerX= ? AND Cancel = ?",winner, 0).Scan(&md)
