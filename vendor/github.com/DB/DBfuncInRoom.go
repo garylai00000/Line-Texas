2ceb4ca3e21612321ac3991cb5904fe679551c2d
garylai00000 <garylai00000@gmail.com>
2016-08-21 01:03:32 +0800

    debug

--- vendor/github.com/DB/DBfuncInRoom.go
+++ vendor/github.com/DB/DBfuncInRoom.go
@@ -55,9 +55,9 @@ func InRoomJoinGame(MID string){
 	bot, _ = linebot.NewClient(numID, os.Getenv("ChannelSecret"), os.Getenv("MID"))
 	db,_ := sql.Open("mysql", os.Getenv("dbacc")+":"+os.Getenv("dbpass")+"@tcp("+os.Getenv("dbserver")+")/")
 	var haveGame int
-	var RID string
+	var RID int
 	var R string
-	var GID string
+	var GID int
 	db.QueryRow("SELECT UserRoom FROM sql6131889.User WHERE MID = ? AND Cancel = ?", MID, 0).Scan(&R)
 	db.QueryRow("SELECT ID FROM sql6131889.Room WHERE  RoomName = ? AND Cancel = ?", R,  0).Scan(&RID)
 	db.QueryRow("SELECT ID FROM sql6131889.Game WHERE RoomID = ? AND Cancel = ?", RID, 0).Scan(&GID)
